e76a4828dc0bd7a81b371abcf4ce3bc5f7077231
Peter Sorotokin <sorotokin@gmail.com>
2007-12-15 22:50:56 +0000

    re-creating trunk

--- jingsrc/com/thaiopensource/relaxng/parse/Include.java
+++
@@ -1,6 +0,0 @@
-package com.thaiopensource.relaxng.parse;
-
-public interface Include extends GrammarSection {
-  void endInclude(String uri, String ns,
-                  Location loc, Annotations anno) throws BuildException, IllegalSchemaException;
-}
--- jingsrc/com/thaiopensource/relaxng/parse/SubParser.java
+++
@@ -1,8 +0,0 @@
-package com.thaiopensource.relaxng.parse;
-
-public interface SubParser {
-  ParsedPattern parseInclude(String uri, SchemaBuilder f, IncludedGrammar g)
-          throws BuildException, IllegalSchemaException;
-  ParsedPattern parseExternal(String uri, SchemaBuilder f, Scope s)
-          throws BuildException, IllegalSchemaException;
-}
