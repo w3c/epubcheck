9130ddf8effe0d15267fb77270d36ad8843e5efe
Paul Norton <cold.geek@gmail.com>
2008-12-09 20:10:25 +0000

    Build 1.0.4 only adds autotest (testing of the validator).

--- com.adobe.epubcheck/src/com/adobe/epubcheck/api/EpubCheck.java
+++ com.adobe.epubcheck/src/com/adobe/epubcheck/api/EpubCheck.java
@@ -36,8 +36,8 @@ import com.adobe.epubcheck.util.WriterReportImpl;
  * Public interface to epub validator.
  */
 public class EpubCheck {
-	/* VERSION number is duplicated in the build.xml file, so you'll need to change it twice */
-	public static final String VERSION = "1.0.3";
+	/* VERSION number is duplicated in the build.xml and war-build.xml files, so you'll need to change it in two additional places */
+	public static final String VERSION = "1.0.4";
 
 	File epubFile;
 
