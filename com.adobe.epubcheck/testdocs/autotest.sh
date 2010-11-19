a41e23da146662eda0ed18513837f23e3fb67322
Liza Daly <lizadaly@gmail.com>
2010-11-19 17:03:05 +0000

    1.1 build

--- com.adobe.epubcheck/testdocs/autotest.sh
+++ com.adobe.epubcheck/testdocs/autotest.sh
@@ -1,1 +1,1 @@
-java -cp ../dist/epubcheck-1.1-rc2.jar com.adobe.epubcheck.autotest.AutoTest ./general/tests.xml ./general/
+java -cp ../dist/epubcheck-1.1.jar com.adobe.epubcheck.autotest.AutoTest ./general/tests.xml ./general/
