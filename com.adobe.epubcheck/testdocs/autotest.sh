0a6262c9703ebf9bde14e0c7d13a32d4532dbeef
George Bina <george@oxygenxml.com>
2011-02-17 01:20:34 +0000

    Fix issue 96 - run the tests from issues/issues.xml and update the issues.xml to be in sync with the results from epubcheck.

--- com.adobe.epubcheck/testdocs/autotest.sh
+++ com.adobe.epubcheck/testdocs/autotest.sh
@@ -1,1 +1,2 @@
 java -cp ../dist/epubcheck-1.1.jar:../lib/jing.jar:../lib/saxon.jar com.adobe.epubcheck.autotest.AutoTest ./general/tests.xml ./general/
+java -cp ../dist/epubcheck-1.1.jar:../lib/jing.jar:../lib/saxon.jar com.adobe.epubcheck.autotest.AutoTest ./issues/issues.xml ./issues/
