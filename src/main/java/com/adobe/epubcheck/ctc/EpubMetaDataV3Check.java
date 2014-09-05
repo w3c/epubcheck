d2adb0463ecbb165d6a7fd8dff84d508510940cf
Romain Deltour <rdeltour@gmail.com>
2014-09-05 11:04:06 +0200

    Remove check OPF_023
    `OPF_23` was checking that there was only one `metadata` element in
    the OPF, but:
    
      * this is already checked by RelaxNG schemas
      * the check is flawed since `collection`s also allow `metadata`
        children

--- src/main/java/com/adobe/epubcheck/ctc/EpubMetaDataV3Check.java
+++ src/main/java/com/adobe/epubcheck/ctc/EpubMetaDataV3Check.java
@@ -1,19 +1,17 @@
 package com.adobe.epubcheck.ctc;
 
+import org.w3c.dom.Document;
+
 import com.adobe.epubcheck.api.Report;
 import com.adobe.epubcheck.ctc.epubpackage.EpubPackage;
-import com.adobe.epubcheck.messages.MessageId;
-import com.adobe.epubcheck.messages.MessageLocation;
 import com.adobe.epubcheck.opf.DocumentValidator;
-import com.adobe.epubcheck.util.EpubConstants;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
 
 public class EpubMetaDataV3Check implements DocumentValidator
 {
 
   private final Document doc;
   private final String pathRootFile;
+  @SuppressWarnings("unused")
   private final Report report;
 
   public EpubMetaDataV3Check(EpubPackage epack, Report report)
@@ -31,17 +29,6 @@ public class EpubMetaDataV3Check implements DocumentValidator
 
   private boolean isMetaDataValid(Document doc, String pathRootFile)
   {
-    boolean isMetadataValid = false;
-    NodeList metadataList = doc.getDocumentElement().getElementsByTagName("metadata");
-    if (metadataList.getLength() == 0)
-    {
-      metadataList = doc.getDocumentElement().getElementsByTagNameNS(EpubConstants.OpfNamespaceUri, "metadata");
-    }
-    if (metadataList.getLength() != 1)
-    {
-      isMetadataValid = false;
-      report.message(MessageId.OPF_023, new MessageLocation(pathRootFile, -1, -1), metadataList.getLength());
-    }
-    return isMetadataValid;
+    return true; // no custom checks
   }
 }
