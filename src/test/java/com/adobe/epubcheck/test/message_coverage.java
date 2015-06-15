2f1daebef2e9aaaf0bcab1813c398810983b80b7
Romain Deltour <rdeltour@gmail.com>
2015-06-15 08:34:26 +0200

    Fix message coverage test
    - Use ordered sets
    - Add un-tested message IDs-

--- src/test/java/com/adobe/epubcheck/test/message_coverage.java
+++ src/test/java/com/adobe/epubcheck/test/message_coverage.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -24,7 +25,7 @@ public class message_coverage
   {
     File dumpFile = new File("ReportedMessages.txt");
     Assert.assertTrue("MessageId output file is missing: " + dumpFile.getAbsolutePath(), dumpFile.exists());
-    HashSet<MessageId> reportedMessageIds = new HashSet<MessageId>();
+    HashSet<MessageId> reportedMessageIds = new LinkedHashSet<MessageId>();
     BufferedReader reader = new BufferedReader(new FileReader(dumpFile));
     Assert.assertNotNull("Could not read the reported message file.", reader);
     String line;
@@ -39,18 +40,19 @@ public class message_coverage
     reader.close();
     List<MessageId> allMessageList = Arrays.asList(MessageId.values());
     Assert.assertNotNull("Could not read the reported message file.", allMessageList);
-    Set<MessageId> allMessages = new HashSet<MessageId>(allMessageList);
+    Set<MessageId> allMessages = new LinkedHashSet<MessageId>(allMessageList);
     allMessages.removeAll(reportedMessageIds);
-    Set<MessageId> expectedMissedCoverage = new HashSet<MessageId>();
+    Set<MessageId> expectedMissedCoverage = new LinkedHashSet<MessageId>();
+    expectedMissedCoverage.add(MessageId.CHK_006); //This message is in a code path that shouldn't ever be hit, but its here in case there is an error parsing the regex result.
+    expectedMissedCoverage.add(MessageId.CHK_007); //This message is in a code path that shouldn't be hit, but is here in case there is some other error accessing the file.
     expectedMissedCoverage.add(MessageId.HTM_002); //This message is in a code path that shouldn't be hit
     expectedMissedCoverage.add(MessageId.HTM_011); //This message may never be reported.  Undeclared entities result in a Sax Parser Error and message RSC_005.
-    expectedMissedCoverage.add(MessageId.CHK_007); //This message is in a code path that shouldn't be hit, but is here in case there is some other error accessing the file.
-    expectedMissedCoverage.add(MessageId.CHK_006); //This message is in a code path that shouldn't ever be hit, but its here in case there is an error parsing the regex result.
+    expectedMissedCoverage.add(MessageId.NAV_001); //This message is only reported if the user explicitly calls validation of a Nav Doc with EPUB 2 version
     expectedMissedCoverage.add(MessageId.OPF_010); //This is only reported in an exception that is difficult to generate in a test
     expectedMissedCoverage.add(MessageId.OPF_011); //This is currently reported as RSC_005 in Schematron, but would be safer in prefix-checked code
-    expectedMissedCoverage.add(MessageId.PKG_015); //This is only reported in an exception that is difficult to generate in a test
     expectedMissedCoverage.add(MessageId.PKG_005); //This is only reported in an exception that is difficult to generate in a test
-    expectedMissedCoverage.add(MessageId.RSC_017); //This message may never be reported.   Sax Parser Error and message RSC_005, but this covers SAX warnings, which may never happen.
+    expectedMissedCoverage.add(MessageId.PKG_015); //This is only reported in an exception that is difficult to generate in a test
+    expectedMissedCoverage.add(MessageId.PKG_023); //TODO add tests
 
     Assert.assertEquals("Messages not covered by tests", expectedMissedCoverage, allMessages);
   }
