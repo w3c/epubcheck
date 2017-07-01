749b0c0607165fe6dcd6a523c2f12f304f728e1c
Thomas Ledoux <tledouxfr@gmail.com>
2017-07-01 20:26:51 +0200

    Add FileName to test and make it work on Windows

--- src/main/java/com/adobe/epubcheck/util/XmlReportAbstract.java
+++ src/main/java/com/adobe/epubcheck/util/XmlReportAbstract.java
@@ -1,5 +1,6 @@
 package com.adobe.epubcheck.util;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.text.CharacterIterator;
 import java.text.SimpleDateFormat;
@@ -224,9 +225,19 @@ public abstract class XmlReportAbstract extends MasterReport {
 		if (path == null || path.length() == 0) {
 			return null;
 		}
+		// Try / because of uris 
 		int lastSlash = path.lastIndexOf('/');
 		if (lastSlash == -1) {
-			return path;
+			if (File.separatorChar != '/') {
+				int lastSlash2 = path.lastIndexOf(File.separatorChar);
+				if (lastSlash2 == -1) {
+					return path;
+				} else {
+					return path.substring(lastSlash2 + 1);
+				}
+			} else {
+				return path;
+			}
 		} else {
 			return path.substring(lastSlash + 1);
 		}
