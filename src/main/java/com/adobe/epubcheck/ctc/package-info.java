b7e39d35b328847f3246363d40908c446eca3ccc
Daniel Persson <mailto.woden@gmail.com>
2017-11-28 06:57:44 +0100

    Added deprecation notice to package.

---
+++ src/main/java/com/adobe/epubcheck/ctc/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package was largely contributed by Barnes & Nobel back in 2013.
+ * This package is scheduled to be reimplemented.
+ *
+ * @author Steve Antoch @ BARNES & NOBEL
+ */
+@Deprecated
+package com.adobe.epubcheck.ctc;
