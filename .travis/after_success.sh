1c95c892ff914d97b08d2b09f7683f28f7fe26cf
Romain Deltour <rdeltour@gmail.com>
2019-01-25 21:18:18 +0100

    chore: change the Maven group ID to `org.w3c`
    - change the group ID in the POM file
    - update the encrypted sonatype credentials in `.travis.yml`
    - only run the Maven deploy command in the first Travis build
    - update the POM version to `4.2.0-beta`
    
    Fixes #960

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_JOB_NUMBER" == *.1 ]]; then
   mvn deploy -DskipTests -Dinvoker.skip=true
 else
   echo "Skipping Maven deploy."
