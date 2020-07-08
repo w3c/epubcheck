3161c9035ede0e980ecd4a8175e11007bbb42adf
Romain Deltour <rdeltour@gmail.com>
2020-07-08 18:15:51 +0200

    test: disable pretty printing of cucumber tests by default

--- src/test/java/org/w3c/epubcheck/test/RunCucumberTest.java
+++ src/test/java/org/w3c/epubcheck/test/RunCucumberTest.java
@@ -6,11 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-    features = "classpath:",
-    strict = true,
-    monochrome = true,
-    plugin = { "pretty" })
+@CucumberOptions(features = "classpath:", strict = true)
 public class RunCucumberTest
 {
 
