8b50dea24f44a88df394b2739bbff045275e7c54
Romain Deltour <rdeltour@gmail.com>
2021-06-30 11:28:28 +0200

    chore: remove Travis config

--- .travis/after_success.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_JOB_NUMBER" == *.1 ]]; then
-  mvn deploy -DskipTests -Dinvoker.skip=true
-else
-  echo "Skipping Maven deploy."
-fi
--- .travis/before_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Copying Maven settings to '$HOME/.m2/settings.xml'"
-cp $TRAVIS_BUILD_DIR/.travis/settings.xml $HOME/.m2/settings.xml
