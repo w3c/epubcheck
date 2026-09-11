9f3d9a5ff58265455dc7ecef666a94830e03c86a
Romain Deltour <rdeltour@gmail.com>
2026-09-11 03:09:29 +0200

    feat: refine `PKG-007` for whitespace mimetype file errors
    Add a localized argument to `PKG-007` to refine its message when the only
    mismatch on the content of the OCF mimetype file is due to whitespace
    issues.
    
    Also reworded the default message to be more understandable.
    
    Fix #1663

--- src/test/java/org/w3c/epubcheck/test/TestReport.java
+++ src/test/java/org/w3c/epubcheck/test/TestReport.java
@@ -93,6 +93,7 @@ public class TestReport extends MasterReport
     MessageInfo messageInfo = new MessageInfo(message.getSeverity(), message.getID(),
         location.getPath(), location.getLine(), location.getColumn(),
         fixMessage(message.getMessage(args)));
+
     if (verbose) System.out.println(messageInfo);
     messages.add(messageInfo);
     // delegate to the formatting report
